6202738cc9439b54408cf4bc707d139435f00960
chaturkurma <chaturk31@gmail.com>
2020-04-28 22:03:13 -0500

    update code in reports

--- Navbar/src/components/reports.js
+++ Navbar/src/components/reports.js
@@ -12,18 +12,63 @@ class Projects extends Component {
 
     if(this.state.activeTab === 0){
       return(
-       
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+           
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
+            
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+
+
+      )
     } else if(this.state.activeTab === 1) {
       return (
-        <div><h1>This is Order Reports</h1></div>
+        <div><h1>This is Production Reports </h1></div>
       )
     } else if(this.state.activeTab === 2) {
       return (
-        <div><h1>View all bar codes</h1></div>
+        <div><h1>This is Orders Reports</h1></div>
       )
     } else if(this.state.activeTab === 3) {
       return (
-        <div><h1>List view by date</h1></div>
+        <div><h1>This is In House Inventory Reports</h1></div>
       )
     }
 
@@ -35,10 +80,10 @@ class Projects extends Component {
     return(
       <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>View all barcodes</Tab>
           <Tab>Production Reports</Tab>
-          <Tab>Order Reports</Tab>
-          <Tab>View all Barcodes</Tab>
-          <Tab>List view by date</Tab>
+          <Tab>Orders Reports</Tab>
+          <Tab>In House Inventory Reports</Tab>
         </Tabs>
 
 
@@ -55,3 +100,4 @@ class Projects extends Component {
 }
 
 export default Projects;
+
