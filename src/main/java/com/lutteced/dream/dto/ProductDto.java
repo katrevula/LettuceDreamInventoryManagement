136be0948f20c5fae69e471910eb5af712c1282b
Prajakt-Khawase <46689825+Prajakt-Khawase@users.noreply.github.com>
2020-05-06 08:49:23 -0500

    validations

--- src/main/java/com/lutteced/dream/dto/ProductDto.java
+++ src/main/java/com/lutteced/dream/dto/ProductDto.java
@@ -12,20 +12,25 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+// Validations
 @Data
 public class ProductDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+       // Error message if field is null
 	@NotNull(message = "{product.name}")
+	
+       // Error message if field is Empty
 	@NotEmpty(message = "{product.name}")
 	private String name;
 
 	@NotNull(message = "{product.code}")
+	// Error message if field is null
 	@NotEmpty(message = "{product.code}")
 	private String code;
-
+        // Error message if field is null
 	@NotNull(message = "{product.expiryTime}")
+	// Error message if field is null
 	@NotEmpty(message = "{product.expiryTime}")
 	private String expiryTime;
 }
